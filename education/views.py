a11111d16cdb34c319533c06f530846c7c2477d8
Мологин Фёдор <proterrarea@gmail.com>
2025-02-09 15:15:26 +0300

    Made all good again

--- education/views.py
+++ education/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 import os
 import markdown
+
+
 def view_course(request,token):
-    md_file_path = os.path.join('courses/', token+'.md')
-    with open(md_file_path, 'r', encoding='utf-8') as f:
-        md_content = f.read()
-    html_content = markdown.markdown(md_content, extensions=['fenced_code', 'codehilite'])
+    lessons = os.listdir('courses/'+token)
+    html_content=[]
+    for i in lessons:
+        with open('courses/'+token+'/' + i, 'r', encoding='utf-8') as f:
+            md_content = f.read()
+            html_content.append(markdown.markdown(md_content, extensions=['fenced_code', 'codehilite']))
     return render(request, 'course.html', {'content': html_content})
 
 
