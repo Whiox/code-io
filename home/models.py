da5c58fea149993d6a4aa64071fc4267c576a6e0
Мологин Фёдор <proterrarea@gmail.com>
2025-02-21 11:15:19 +0300

    Информация в профиле

--- home/models.py
+++ home/models.py
@@ -8,3 +8,16 @@ class UserProfile(models.Model):
     about = models.TextField(default='Не указано')
     email = models.TextField(default='Не указано') # Контактный email
     phone = models.TextField(default='Не указано')
+
+
+class SocialNetwork(models.Model):
+    network_id = models.AutoField(primary_key=True)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    label = models.TextField(null=False)
+    linc = models.TextField(null=False)
+
+
+class Interest(models.Model):
+    interest_id = models.AutoField(primary_key=True)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    label = models.TextField(null=False)
--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.utils import timezone
 from django.http import JsonResponse
-from home.models import UserProfile
+from home.models import UserProfile, SocialNetwork, Interest
 
 def home_view(request):
     current_time = timezone.now().strftime('%Y-%m-%d %H:%M:%S.%f %Z')
@@ -22,7 +22,9 @@ class MyProfileView(View):
 
         content = {
             'username': request.user.username,
-            'user_profile': user_profile
+            'user_profile': user_profile,
+            'social_network': SocialNetwork.objects.filter(user_profile=user_profile),
+            'interest': Interest.objects.filter(user_profile=user_profile)
         }
 
         return render(request, 'profile.html', content)
