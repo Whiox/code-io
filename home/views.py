93c5e2c180a08eadbb41428f7bbd9f8632f56c18
Мологин Фёдор <proterrarea@gmail.com>
2025-02-22 23:32:13 +0300

    Модальное окно только для владельцев

--- home/views.py
+++ home/views.py
@@ -42,7 +42,8 @@ class MyProfileView(View):
             'username': request.user.username,
             'user_profile': user_profile,
             'social_network': SocialNetwork.objects.filter(user_profile=user_profile),
-            'interest': Interest.objects.filter(user_profile=user_profile)
+            'interest': Interest.objects.filter(user_profile=user_profile),
+            'is_owner': True
         }
 
         return render(request, 'profile.html', content)
