dc3046e125e93e1f55c3cd40350ba3f17a5639a0
Мологин Фёдор <proterrarea@gmail.com>
2025-05-14 17:43:03 +0300

    Reverse

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,10 @@
 """Формы аутентификации."""
 
 from django import forms
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+from authentication.models import User
 
 
 class RegisterForm(forms.Form):
@@ -12,7 +16,8 @@ class RegisterForm(forms.Form):
     :ivar forms.CharField repeat_password: Повтор пароля
     """
     username = forms.CharField(
-        label='', max_length=255,
+        label='',
+        max_length=150,
         widget=forms.TextInput(attrs={
             'class': 'auth-form-control',
             'placeholder': 'Введите имя пользователя'
@@ -20,25 +25,58 @@ class RegisterForm(forms.Form):
     )
     email = forms.EmailField(
         label='',
-        widget=forms.TextInput(attrs={
+        widget=forms.EmailInput(attrs={
             'class': 'auth-form-control',
             'placeholder': 'Электронная почта'
         })
     )
     password = forms.CharField(
+        label='',
+        strip=False,
         widget=forms.PasswordInput(attrs={
             'class': 'auth-form-control',
             'placeholder': 'Введите пароль',
             'autocomplete': 'new-password'
-        })
+        }),
+        help_text="Пароль должен соответствовать требованиям безопасности."
     )
     repeat_password = forms.CharField(
+        label='',
+        strip=False,
         widget=forms.PasswordInput(attrs={
             'class': 'auth-form-control',
             'placeholder': 'Повторите пароль',
             'autocomplete': 'new-password'
         })
     )
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Пользователь с таким именем уже существует.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Пользователь с такой почтой уже зарегистрирован.")
+        return email
+
+    def clean_password(self):
+        pwd = self.cleaned_data.get('password')
+        try:
+            validate_password(pwd)
+        except ValidationError as exc:
+            raise ValidationError(exc.messages)
+        return pwd
+
+    def clean(self):
+        cleaned_data = super().clean()
+        pwd = cleaned_data.get('password')
+        pwd2 = cleaned_data.get('repeat_password')
+        if pwd and pwd2 and pwd != pwd2:
+            self.add_error('repeat_password', "Пароли не совпадают.")
+        return cleaned_data
 
 
 class LoginForm(forms.Form):
