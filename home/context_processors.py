9a7e0bbbd7af1cb3db03521e40b4a133a7778fab
Мологин Фёдор <proterrarea@gmail.com>
2025-05-16 11:45:16 +0300

    Fix context_processors.py

--- home/context_processors.py
+++ home/context_processors.py
@@ -2,7 +2,7 @@ from home.models import UserProfile
 
 
 def user_avatar(request):
-    if request.user:
+    if request.user.is_authenticated:
         profile_owner = request.user
         user_profile, _ = UserProfile.objects.get_or_create(user=profile_owner)
 
